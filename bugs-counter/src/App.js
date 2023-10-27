64a133330cb54bed3e4f3dd5d1312d51ca0427e2
gorunovamaria <goryunova_m@tochka.com>
2023-10-27 22:49:57 +0500

    Add new module calculations

--- bugs-counter/src/App.js
+++ bugs-counter/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
+import Calculation from "./Сalculation";
 
 function App() {
-  const [cycleCount, setCountCycle] = React.useState(Number(localStorage.getItem('cycles'))||0);
-  const [bugsCount, setCountBugs] = React.useState(Number(localStorage.getItem('bugs'))||0);
-  const [task, setTaskToInput] = React.useState(localStorage.getItem('task')||'');
+  const [calculationKey, setcalculationKey] = useState(0);
+
+  const [cycleCount, setCountCycle] = useState(Number(localStorage.getItem('cycles'))||0);
+  const [bugsCount, setCountBugs] = useState(Number(localStorage.getItem('bugs'))||0);
+  const [task, setTaskToInput] = useState(localStorage.getItem('task')||'');
 
   const cycleCountPlus = () => {
     setCountCycle(cycleCount + 1);
@@ -25,41 +28,49 @@ function App() {
   const clearCounts = () => {
     // eslint-disable-next-line
     if (confirm("Уверены что закончили с этой задачей?")){
-      localStorage.clear('cycles', 'bugs', 'task');
+      localStorage.clear();
       localStorage.setItem('cycles', String(0));
       localStorage.setItem('bugs', String(0));
       localStorage.setItem('task', '');
+      localStorage.setItem('taskSize', '');
+      localStorage.setItem('caseAmount', String(0));
+      localStorage.setItem('timeAmount', String(0));
+      //setTaskSize('?');
+      //setCaseAmountToInput('');
+      //setTimeAmountToInput('');
       setCountCycle(0);
       setCountBugs(0);
       setTaskToInput('');
+      setcalculationKey(calculationKey+1);
     }
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <Calculation key={calculationKey} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         <code>Bugs Counter Helper</code>
         </p>
-        <p><a
-          className="App-link"
-          href="https://www.techtarget.com/searchsoftwarequality/definition/bug#:~:text=In%20computer%20technology%2C%20a%20bug,is%20manufactured%20into%20a%20microprocessor"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn more about bugs
-        </a>
-        </p>
+
         <p className="Button-line">
           <div><button onClick={cycleCountPlus} className="Button">Кол-во циклов</button><h2>{cycleCount}</h2></div>
           <div><button onClick={bugsCountPlus} className="Button">Кол-во багов</button><h2>{bugsCount}</h2></div>
         </p>
         <div>
-          <p>Ссылка на задачу:</p>
-          <input type="url" value={task} onChange={setTask} className="Input"/>
+          <input type="url" value={task} onChange={setTask} placeholder="Ссылка на задачу" className="Input"/>
           <div><button onClick={clearCounts} className="Button">Приступить к новой задаче</button></div>
         </div>
+        <p><a
+            className="App-link"
+            href="https://www.techtarget.com/searchsoftwarequality/definition/bug#:~:text=In%20computer%20technology%2C%20a%20bug,is%20manufactured%20into%20a%20microprocessor"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          Learn more about bugs
+        </a>
+        </p>
       </header>
     </div>
   );
---
+++ bugs-counter/src/Сalculation.js
@@ -0,0 +1,56 @@
+import './Calculation.css';
+import React, { useState, useEffect }  from "react";
+
+function Calculation() {
+    const [taskSize, setTaskSize] = useState(localStorage.getItem('taskSize')||0);
+    const [caseAmount, setCaseAmountToInput] = useState(Number(localStorage.getItem('caseAmount'))||0);
+    const [timeAmount, setTimeAmountToInput] = useState(Number(localStorage.getItem('timeAmount'))||0);
+
+    useEffect(() => {
+        document.title = `Your task size is ${taskSize}`;
+    }, [taskSize]);
+
+    const calculate = () => {
+        let result = caseAmount * timeAmount * 2 / 60 / 8;
+        console.log(result)
+        let calculatedTaskSize;
+
+        if (result < 1) {
+            calculatedTaskSize = 'XXS';
+        } else if (result>=1 && result < 2) {
+            calculatedTaskSize = 'XS';
+        } else if (result>=2 && result < 3) {
+            calculatedTaskSize = 'S';
+        } else if (result >=3 && result < 5) {
+            calculatedTaskSize = 'M';
+        } else if (result >= 5 && result < 10) {
+            calculatedTaskSize = 'L';
+        } else if (result >= 10  && result < 11) {
+            calculatedTaskSize = 'XL';
+        } else {
+            calculatedTaskSize = 'XXL';
+        }
+
+        setTaskSize(calculatedTaskSize);
+    }
+
+    const setCaseAmount = (event) => {
+        setCaseAmountToInput(event.target.value);
+        localStorage.setItem('caseAmount', event.target.value);
+    };
+
+    const setTimeAmount = (event) => {
+        setTimeAmountToInput(event.target.value);
+        localStorage.setItem('timeAmount', event.target.value);
+    };
+
+    return(
+        <div className="Calculation">
+            <input type="text" value={caseAmount||''} onChange={setCaseAmount} placeholder="Количество кейсов, шт" className="Input-amount"/><br/>
+            <input type="text" value={timeAmount||''} onChange={setTimeAmount} placeholder="Примерное время на кейс, мин" className="Input-amount"/><br/>
+            <div><button onClick={calculate} className="Button">Рассчитать</button><h2>{taskSize}</h2></div>
+        </div>
+    );
+}
+
+export default Calculation;
