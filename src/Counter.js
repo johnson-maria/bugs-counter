c0d3de960dc47e0f8f20362e2570615e3bbf3284
gorunovamaria <goryunova_m@tochka.com>
2024-03-29 22:59:38 +0500

    logging counts

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,10 @@
 import './Counter.css';
-import React, { useState }  from "react";
+import React, {useState} from "react";
 
 export function Counter() {
 
-    const [cycleCount, setCountCycle] = useState(Number(localStorage.getItem('cycles'))||0);
-    const [bugsCount, setCountBugs] = useState(Number(localStorage.getItem('bugs'))||0);
+    const [cycleCount, setCountCycle] = useState(Number(localStorage.getItem('cycles')) || 0);
+    const [bugsCount, setCountBugs] = useState(Number(localStorage.getItem('bugs')) || 0);
 
     const cycleCountPlus = () => {
         setCountCycle(cycleCount + 1);
@@ -16,17 +16,19 @@ export function Counter() {
         localStorage.setItem('bugs', String(bugsCount + 1));
     };
 
-    return(
+    console.log(cycleCount, bugsCount)
 
+    return (
         <div className="Button-line">
             <div className="Cycles-counters">
                 <div>
                     <button onClick={cycleCountPlus} className="Button">Добавить цикл</button>
                     <h2>{cycleCount}</h2>
                 </div>
-                <div><button onClick={bugsCountPlus} className="Button">Добавить баг</button><h2>{bugsCount}</h2></div>
+                <div>
+                    <button onClick={bugsCountPlus} className="Button">Добавить баг</button>
+                    <h2>{bugsCount}</h2></div>
             </div>
         </div>
-
     );
 }
